4799fd2df759c1f3bf2cce87f625bab679f68bda
Yeonju Seo <yeonjuseo712@gmail.com>
2024-11-04 18:44:43 +0900

    관심상품 희망최저가 업데이트 api 구현

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -1,13 +1,11 @@
 package com.sparta.myselectshop.controller;
 
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,7 +21,12 @@ public class ProductController {
         return productService.createProduct(requestDto);
     }
 
-
+    // 관심 상품 희망 최저가 등록하기
+    @PutMapping("/products/{id}")
+    public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
+        // 응답 보내기
+        return productService.updateProduct(id, requestDto);
+    }
 
 }
 
--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -2,10 +2,12 @@ package com.sparta.myselectshop.service;
 
 //import com.sparta.myselectshop.dto.ProductMypriceRequesDto;
 //import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.repository.ProductRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,16 +20,30 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    public static final int MIN_MY_PRICE = 100;
+
     // Entity 객체 생성 및 DB 저장
     public ProductResponseDto createProduct(ProductRequestDto requestDto) {
         Product product = productRepository.save(new Product(requestDto));
         return new ProductResponseDto(product);
     }
 
-    // 관심 상품 희망 최저가 등록하기
-//    @PutMapping("/products/{id}")
-//    public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
-//        // 응답 보내기
-//        return productService.updateProduct(id, requestDto);
-//    }
+    @Transactional
+    public ProductResponseDto updateProduct(Long id, ProductMypriceRequestDto requestDto) {
+        int myprice = requestDto.getMyprice();
+        if (myprice < MIN_MY_PRICE) {
+            throw new IllegalArgumentException("유효하지 않은 관심 가격입니다. 최소 " + MIN_MY_PRICE + " 원 이상으로 설정해 주세요.");
+        }
+
+        Product product = productRepository.findById(id).orElseThrow(() ->
+                new NullPointerException("해당 상품을 찾을 수 없습니다.")
+        );
+
+        product.update(requestDto);
+
+        return new ProductResponseDto(product);
+
+    }
+
+
 }
