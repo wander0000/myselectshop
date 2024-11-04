0023304c79095274ee7d0e9b6f226675e5ae9925
Yeonju Seo <yeonjuseo712@gmail.com>
2024-11-04 18:36:50 +0900

    관심상품 등록 api 구현(controller 수정)

--- src/main/java/com/sparta/myselectshop/naver/controller/NaverApiController.java
+++ src/main/java/com/sparta/myselectshop/naver/controller/NaverApiController.java
@@ -17,19 +17,10 @@ import java.util.List;
 public class NaverApiController {
 
     private final NaverApiService naverApiService;
-    private final ProductService productService;
 
     @GetMapping("/search")
     public List<ItemDto> searchItems(@RequestParam String query)  {
         return naverApiService.searchItems(query);
     }
-
-    // 관심 상품 등록하기
-    @PostMapping("/products")
-    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) {
-        // 응답 보내기
-        return productService.createProduct(requestDto);
-    }
-
 
 }
